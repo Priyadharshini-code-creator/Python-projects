8f7ca844d6bd6cd2c1f31ed179ad489054711a61
Priyadharshini-code-creator <priyadharshini.techcareer@gmail.com>
2025-08-05 21:47:03 +0530

    basic changes

--- Library Management System.py
+++ Library Management System.py
@@ -1,5 +1,21 @@
-print("1. Add Book")
-print("2. Display Books")
-print("3. Borrow Book")
-print("4. Return Book")
-Print("5. Exit")
+while True:
+    print(" ---- Library Menu ----\n")
+    print("1. Add Book")
+    print("2. Display Books")
+    print("3. Borrow Book")
+    print("4. Return Book")
+    print("5. Exit\n")
+    User_input=input("Enter your choice(1-5):")
+    if(User_input=='1')
+
+ class books:
+    def __init__(self,Author,Title)
+    self.Author=Author
+    self.Title=Title
+    self.is_available="True"
+
+class library(books):
+    def_init
+
+
+    
