a66aafb7ee72fea4f1accfb28bb92d13915a2afe
Priyadharshini-code-creator <priyadharshini.techcareer@gmail.com>
2025-08-06 21:35:01 +0530

    defining functions

--- Library Management System.py
+++ Library Management System.py
@@ -1,28 +1,33 @@
-while True:
-    print(" ---- Library Menu ----\n")
-    print("1. Add Book")
-    print("2. Display Books")
-    print("3. Borrow Book")
-    print("4. Return Book")
-    print("5. Exit\n")
-    User_input=input("Enter your choice(1-5):")
-    if(User_input=='1'):
-        
-
- class books:
-    def __init__(self,Author,Title):
-       self.Author=Author
-       self.Title=Title
-       self.is_available="True"
-
+class book:
+    def __init__(self,author,title):
+        self.author=author
+        self.title=title
+        self.is_available=True
     def __str__(self):
-        return f'{self.Title} by {self.Author}'-{"Available" if self.is_available else "Not Available"}
+        return f'{self.title} by {self.author}-{'Available' if self.available=True else 'Not Avaiable'}
 
 class library(self):
     self.books=[]
-    def add_books(self,Author,Title):
-        new_book=books(Author,Title)
+
+    def add_book(self,author,title):
+        new_book=book(author,title)
         self.books.append(new_book)
 
+    def display_book(self):
+        if not self.books:
+            print("Books are not Available")
+        else:
+            for idx,books in enumerate(self.books,start=1):
+                print(f'{idx}.{books}')
 
-    
+    def borrow_book(self,index):
+        if 0<index<len(self.books):
+            book=self.books[index-1]
+            if self.is_available:
+                self.is_available=False
+                print(f'You have successfully borrowed the book - {book}')
+            else:
+                print("This book is already borrowed and not returned yet!")
+            
+        else:
+            print("Please enter the correct book number!")
